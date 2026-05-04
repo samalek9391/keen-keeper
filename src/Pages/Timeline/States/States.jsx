90b2596d5adfb0c32143ac7a3f8503d4730cc18f
samalek9391 <sa.malek9391@gmail.com>
2026-05-04 18:36:15 +0600

    stats page is added

--- src/Pages/Timeline/States/States.jsx
+++ src/Pages/Timeline/States/States.jsx
@@ -1,21 +1,77 @@
-import React from 'react';
-import { Pie, PieChart } from 'recharts';
+import React, { useContext } from 'react';
+import { Cell, Legend, Pie, PieChart, ResponsiveContainer, Tooltip } from 'recharts';
+import { TimelineContext } from '../../../Context/TimelineContext';
 
 
-const data = [
-  { name: "Call", value: 4 },
-  { name: "Text", value: 2 },
-  { name: "Video", value: 3 }
-];
+
 
 const States = () => {
-    return (
-    <div className="p-4">
-      <h1>Friendship Analytics</h1>
 
-      <PieChart width={300} height={300}>
-        <Pie data={data} dataKey="value" outerRadius={100} />
-      </PieChart>
+  const { timeline } = useContext(TimelineContext);
+
+  // 🎯 Count interactions
+  const counts = {
+    Call: 0,
+    Text: 0,
+    Video: 0,
+  };
+
+  timeline.forEach((item) => {
+    if (counts[item.type] !== undefined) {
+      counts[item.type]++;
+    }
+  });
+
+
+
+    const data = [
+    { name: "Text", value: counts.Text },
+    { name: "Call", value: counts.Call },
+    { name: "Video", value: counts.Video },
+  ];
+
+
+
+    const COLORS = ["#7C3AED", "#1F2937", "#22C55E"];
+
+
+    return (
+    <div className="p-6 bg-gray-50 min-h-screen">
+      
+      {/* Heading */}
+      <h1 className="text-2xl text-[#1F2937] font-semibold mb-6">
+        Friendship Analytics
+      </h1>
+
+      {/* Card */}
+      <div className="bg-white rounded-xl shadow p-6 max-w-3xl my-10 mx-auto">
+        
+        <h3 className="mb-4 text-xl text-[#244D3F]">
+          By Interaction Type
+        </h3>
+
+        {/* Chart */}
+        <div className="w-full h-75">
+          <ResponsiveContainer>
+            <PieChart>
+              <Pie
+                data={data}
+                innerRadius={70}
+                outerRadius={100}
+                paddingAngle={5}
+                dataKey="value"
+              >
+                {data.map((entry, index) => (
+                  <Cell key={index} fill={COLORS[index]} />
+                ))}
+              </Pie>
+
+              <Tooltip />
+              <Legend />
+            </PieChart>
+          </ResponsiveContainer>
+        </div>
+      </div>
     </div>
     );
 };
